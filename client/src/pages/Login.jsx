8974109a87010636439c6ee16ba66bdf19edb99b
Nithin Kumar Appari <52022957+nithin3q@users.noreply.github.com>
2024-02-13 13:38:01 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
       };
       try {
         const response = await axios.post(
-          "http://localhost:3000/api/v1/login",
+          "https://task-manager-mu-lime.vercel.app/api/v1/login",
           formData
         );
         localStorage.setItem("auth", JSON.stringify(response.data.token));
